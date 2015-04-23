548300482e305f4cbe5326b28063ff6fe917e463
nexusriot <nexus.riot@gmail.com>
2015-04-23 12:15:15 +0300

    Exception handling in tests.post

--- tests/post.py
+++ tests/post.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib
 import os
 import requests
 
 
-def post():
-    payload = {'@number': 12345}
-    r = requests.post('http://localhost:5000/test', data=payload)
-    return r.status_code
+def post(url='http://localhost:5000/test', payload={'@number': 12345}):
+    code = 0
+    try:
+        r = requests.post(url, data=payload)
+        code = r.status_code
+    except requests.exceptions.ConnectionError as e:
+        code = e.args[0].reason.errno
+    return code
 
 
-def post_multipart():
-    payload = {'@number': 1}
-    url = 'http://localhost:5000/test'
-    files = {'file': open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'testfile')), 'rb')}
-    r = requests.post(url, files=files, data=payload)
-    return r.status_code
+def post_multipart(url='http://localhost:5000/test', payload={'@number': 1},
+                   file_name=os.path.abspath(os.path.join(os.path.dirname(__file__), 'testfile'))):
+    code = 0
+    try:
+        files = {'file': open(file_name, 'rb')}
+        try:
+            pass
+            r = requests.post(url, files=files, data=payload)
+            code = r.status_code
+        except requests.exceptions.ConnectionError as e:
+            code = e.args[0].reason.errno
+    except IOError:
+        code = -1
+    return code
 
 
 def main():
-    post()
-    post_multipart()
+    print (post())
+    print (post_multipart())
 
 
 if __name__ == '__main__':
