884e8b19c88994d3488cc3911aecdd7408b43300
nexusriot <nexus.riot@gmail.com>
2015-05-05 12:20:12 +0300

    created get_file_names()

--- modules/utils.py
+++ modules/utils.py
@@ -4,11 +4,23 @@
 import os
 import stat
 import time
+from os import listdir
+from os.path import isfile, isdir, join
 
 
 def get_file_info(file_name):
-    file_format = "%Y %m %d %H:%M:%S"
+    file_format = "%Y-%m-%d %H:%M:%S"
     stats = os.stat(file_name)
     modification_time = time.strftime(file_format, time.localtime(stats[stat.ST_MTIME]))
     access_time = time.strftime(file_format, time.localtime(stats[stat.ST_ATIME]))
     return modification_time, access_time
+
+
+def get_file_names(path):
+    if isdir(path):
+        return [os.path.join(path, f) for f in listdir(path) if isfile(join(path, f))]
+    return []
+
+
+
+
---
+++ uploader_tests/database.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+from server import db, EventType, Event
+from modules.utils import get_file_names
+
+
+
+
+
+
